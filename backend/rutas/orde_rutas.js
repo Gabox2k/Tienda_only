83828b26ca1a2817512d7bd40a0cf190cfd648d6
Gabriel Raidan <sforza1383@gmail,com>
2026-01-08 11:02:49 -0300

    se corrigio algunos errores

--- backend/rutas/orde_rutas.js
+++ backend/rutas/orde_rutas.js
@@ -9,4 +9,16 @@ ruta.get("/", auth, async (req, res) => {
     res.render("orders", {orders });
 });
 
+ruta.post("/crear", async (req, res) => {
+
+ const { productos, direccion } = req.body;
+
+  if (!productos) return res.status(400).json({ ok: false, msg: "No hay productos" });
+
+  await Orde.create({ productos, addres: direccion });
+
+  res.status(200).json({ ok: true });
+});
+
+
 export default ruta
